a67b1dee82267a7c60eebf96899a2432e024286b
jutionck <jutionck@gmail.com>
2022-11-07 11:40:24 +0700

    add: Material add - Type Conversions

---
+++ type-conversions/index.js
@@ -0,0 +1,44 @@
+let strNum = '10';
+let num = 5;
+console.log(num);
+let result = strNum + num;
+console.log(result); // 105
+
+// Other
+let numStr = String(10)
+console.log(numStr) // "10"
+
+let boolStr = String(true)
+console.log(boolStr) // "true"
+
+let int2 = Number(10)
+
+let num1 = Number("1")
+console.log(num1) // 1
+
+let num2 = Number("2.3")
+console.log(num2) // 2.3
+
+let num3 = Number(false)
+console.log(num3) // 0
+
+let num4 = Number(true)
+console.log(num4) // 1
+
+let num5 = parseInt("10")
+console.log(num5) // 10
+
+let num6 = parseFloat("10.2")
+console.log(num6) // 10.2
+
+let toBool1 = Boolean(1)
+console.log(toBool1) // true
+
+let toBool2 = Boolean(0)
+console.log(toBool2) // false
+
+let toBool3 = Boolean("")
+console.log(toBool3) // false
+
+let toBool4 = Boolean(" ")
+console.log(toBool4) // true
